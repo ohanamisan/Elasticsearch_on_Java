5bdb8924008f272b86f14abb6dc3ac4743225e86
hideyuki-aoki <aoki.ba4@gmail.com>
2018-12-16 23:28:49 +0900

    BulkConfigの責務をBulkAPIの設定に絞り、 起動時処理をアプリエントリーポイントに移動

--- src/main/java/com/aoking/ElasticSearchOnJava/ElasticSearchOnJavaApplication.java
+++ src/main/java/com/aoking/ElasticSearchOnJava/ElasticSearchOnJavaApplication.java
@@ -1,12 +1,69 @@
 package com.aoking.ElasticSearchOnJava;
 
+import com.aoking.ElasticSearchOnJava.config.BulkConfig;
+import com.aoking.ElasticSearchOnJava.config.ClientConfig;
+import com.aoking.ElasticSearchOnJava.entity.Entry;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class ElasticSearchOnJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ElasticSearchOnJavaApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(ElasticSearchOnJavaApplication.class, args);
+		ElasticSearchOnJavaApplication app = ctx.getBean(ElasticSearchOnJavaApplication.class);
+		app.initData();
+	}
+
+	private final ObjectMapper jsonMapper = new ObjectMapper();
+	private final String INDEX = "qiita";
+	private final String TYPE  = "entry";
+
+	/**
+	 * アプリ起動時にQiitaから記事を500件取得します
+	 */
+	public void initData(){
+		try (RestHighLevelClient client = ClientConfig.connectRest();
+			 BulkProcessor bulk = BulkConfig.getBulk(client)){
+
+			for (int i = 1; i <= 5; i++) {
+
+				URL url = new URL("https://qiita.com/api/v2/items?page="+i+"&per_page=100");
+
+				HttpsURLConnection urlconn = (HttpsURLConnection) url.openConnection();
+				urlconn.setRequestMethod("GET");
+				urlconn.setInstanceFollowRedirects(false);
+				urlconn.connect();
+
+				BufferedReader reader = new BufferedReader(new InputStreamReader(urlconn.getInputStream()));
+				List<Entry> entryList = Arrays.asList(jsonMapper.readValue(reader.readLine(), Entry[].class));
+				for (Entry entry : entryList) {
+					Map<String, Object> map = new HashMap<>();
+					map.put("id", entry.getId());
+					map.put("title", entry.getTitle());
+					map.put("body", entry.getBody());
+					map.put("url", entry.getUrl());
+					bulk.add(new IndexRequest(INDEX, TYPE, entry.getId()).source(map));
+				}
+
+				urlconn.disconnect();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/aoking/ElasticSearchOnJava/config/BulkConfig.java
+++ src/main/java/com/aoking/ElasticSearchOnJava/config/BulkConfig.java
@@ -1,12 +1,9 @@
 package com.aoking.ElasticSearchOnJava.config;
 
-import com.aoking.ElasticSearchOnJava.entity.Entry;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.elasticsearch.action.bulk.BackoffPolicy;
 import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
-import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.ByteSizeUnit;
@@ -14,25 +11,13 @@ import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.unit.TimeValue;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Component
 public class BulkConfig {
 
-    private final ObjectMapper jsonMapper = new ObjectMapper();
-    private final String INDEX = "qiita";
-    private final String TYPE  = "entry";
-
-    private BulkProcessor getBulk(){
-        RestHighLevelClient client = ClientConfig.connectRest();
+    /**
+     * BulkAPIの基本設定を取得します
+     */
+    public static BulkProcessor getBulk(RestHighLevelClient client){
         BulkProcessor.Listener listener = new BulkProcessor.Listener() {
             @Override
             public void beforeBulk(long executionId, BulkRequest request) {
@@ -61,38 +46,5 @@ public class BulkConfig {
                             .setBackoffPolicy(BackoffPolicy.exponentialBackoff(TimeValue.timeValueMillis(100), 3))
                             .build();
     }
-
-
-    /**
-     * アプリ起動時にQiitaの記事をElasticsearchにインサートします
-     */
-    @PostConstruct
-    public void initData() {
-        try {
-            for (int i = 1; i <= 5; i++) {
-
-                URL url = new URL("https://qiita.com/api/v2/items?page="+i+"&per_page=100");
-
-                HttpsURLConnection urlconn = (HttpsURLConnection) url.openConnection();
-                urlconn.setRequestMethod("GET");
-                urlconn.setInstanceFollowRedirects(false);
-                urlconn.connect();
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(urlconn.getInputStream()));
-                List<Entry> entryList = Arrays.asList(jsonMapper.readValue(reader.readLine(), Entry[].class));
-                for (Entry entry : entryList) {
-                    Map<String, Entry> map = new HashMap<>();
-                    map.put("entry", entry);
-                    getBulk().add(new IndexRequest(INDEX, TYPE, entry.getId()).source(map));
-
-                }
-
-                urlconn.disconnect();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
 }
